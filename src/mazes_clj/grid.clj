69da3303efdf60cb297c8b7e3c5d317daad40fde
Andy Marks <vampwillow@gmail.com>
2021-12-24 15:34:26 +1100

    + Grid init of cells

--- src/mazes_clj/grid.clj
+++ src/mazes_clj/grid.clj
@@ -1,5 +1,10 @@
 (ns mazes-clj.grid
+  (:require [mazes-clj.cell :as cell])
   (:gen-class))
 
+(defn init-cells [rows columns]
+  (for [r (range rows)]
+    (map (fn [c] (cell/init r c)) (range columns))))
+
 (defn init [rows columns]
-  {:rows rows :columns columns})
+  {:rows rows :columns columns :cells (init-cells rows columns)})
--- test/mazes_clj/grid_test.clj
+++ test/mazes_clj/grid_test.clj
@@ -1,6 +1,7 @@
 (ns mazes-clj.grid-test
   (:require [clojure.test :refer :all]
-            [mazes-clj.grid :refer :all]))
+            [mazes-clj.grid :refer :all]
+            [mazes-clj.cell :as cell]))
 
 (deftest grids
 
@@ -9,4 +10,10 @@
       (is (= 4 (:rows g)))
       (is (= 5 (:columns g)))))
 
-  (testing "can calculate relative distances"))
+  (testing "are initiaised with cells"
+    (let [g (init 1 2)
+          c (:cells g)
+          row1 (first c)]
+      (is (not-empty c))
+      (is (= (cell/init 0 0) (nth row1 0)))
+      (is (= (cell/init 0 1) (nth row1 1))))))
